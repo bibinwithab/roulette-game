006b7039921f2443c1d1f4e6f8e3ef301c83caeb
bibinwithab <bibingraceson@gmail.com>
2023-06-01 18:18:17 +0530

    Updated user interface

--- roulette.py
+++ roulette.py
@@ -5,14 +5,21 @@ print("""Rules:
 
 import random
 
-bullet = random.randint(1, 6)
-count = 0
-shots_fired = 6
-while count < shots_fired:
-    guess = int(input("Enter a number from one to six: "))
-    count += 1
-    if guess == bullet:
-        print("YOU DIED!")
-        break
-    else:
-        print("You live to see another day.")
+def game():
+    bullet = random.randint(1, 6)
+    count = 0
+    shots_fired = 6
+    while count < shots_fired:
+        guess = int(input("Enter a number from one to six: "))
+        count += 1
+        if guess == bullet:
+            print("YOU DIED!")
+            break
+        else:
+            print("You live to see another day.")
+
+ans = input("Wanna contiue? (y/n)").lower()
+if ans == "y":
+    game()
+else:
+    print("Bye!")
